63156a6d42028839060797141edd63b75d80a4d2
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-05 23:14:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ from collections import defaultdict
 from typing import Dict, List, Union
 from pyrogram import Client
 
-LOG_CHANNEL = environ.get('LOG', '')
+LOG_CHANNEL = environ.get('LOG', '-1001684591826')
 
-ACC_SND_LOG =environ.get('SND', '')
+ACC_SND_LOG =environ.get('SND', 'on')
                          
-ACC_DB = environ.get('DB', '')
+ACC_DB = environ.get('DB', 'mongodb+srv://Mst:Mst@cluster0.g3uxpl0.mongodb.net/?retryWrites=true&w=majority')
 

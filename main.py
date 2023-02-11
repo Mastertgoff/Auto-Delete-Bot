d21923f7b129096a64165901a15ce1e7c4a63e94
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-11 14:52:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ async def bcast(_, m : Message):
     await lel.edit(f"✅Successfull to `{success}` users.\n❌ Faild to `{failed}` users.\n👾 Found `{blocked}` Blocked users \n👻 Found `{deactivated}` Deactivated users.")
 
     if query.data == "time":
-        await query.answer(hello) 
+        await query.answer(hello, show_alert=True) 
         
 Bot.start()
 print("Bot Started!")
